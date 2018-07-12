28852939f1bc37cb4ec0edd2c5e714bbb7f48740
Tin <krstnpangan@gmail.com>
2018-07-12 21:37:35 +0800

    Added code for exercise_07

---
+++ exercise_07.rb
@@ -0,0 +1,7 @@
+book_card = {
+  :title          => "The Mysterious Affair at Styles",
+  :author         => "Agatha Christie",
+  :year_published => "1920",
+  :genre          => "Crime Novel"
+}
+puts book_card
